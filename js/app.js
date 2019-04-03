2fdda18fcc27a32fee4fa93d3ad62a22a7ef1a9b
Jodeilson Lima <jodeilsonlima@gmail.com>
2019-04-03 18:55:47 -0400

    Alteração

--- js/app.js
+++ js/app.js
@@ -1,31 +1,66 @@
 // function consultar()
 // {
 //const url = 'https://servicos.detran.ro.gov.br/auth/api/token';
-const url = "https://servicos.detran.ro.gov.br/renavam/api/vistoria/consulta/ncz2550";
-//const url = 'https://reqres.in/api/users';
+const url = "https://servicos.detran.ro.gov.br/renavam/api/vistoria/consulta/aaa9999";
+const get_token = "https://servicos.detran.ro.gov.br/auth/api/token";
 
-let usuario = {
-    login:'otimisa',
-    senha:'12345'
-};
+let token;
+let html;
 
-    fetch(url,
+let usuario = [
+     login ='otimisa',
+     senha = '12345'
+];
+
+
+fetch(get_token,
     {
         headers: {
-           'Authorization':'v-aXEmCzBK50BXWA3uMcaHiOOmo4YaNz1EIZIIRktA7hD7jv-KBIsMU9RWihHNea2yPs6fOMbEcUbLEtUV0sXCcniNkIGPf8iZNLswxmeaQKygmArwU6l5DjUtCRnlgQaCBA-Br-vglrH1nEJpOh40EUfPD79L_A_0MM23QSFHx6yQIPCg5ELf0o-E2ybE2i6l8YGO_YBk2kl2_Wraoqt70MA-8HqeUagwDGzO_YuOSZxff4cKd_WGMXWebtTcIIID47rsIR_nGIb8WY1iXqnRoeqaEqILwVOBqgyz0Ky0I2gjudFXeTvJKOb98IQaH8',
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        method: "GET",
-        //body: JSON.stringify({usuario})
+        method: "POST",
+       // body: JSON.stringify(usuario)
+        //body: JSON.stringify(usuario)
+        body: 'login=administrador&senha=12345'
     })
-    //.then(function(res){ console.log(res) })
+//.then(function(res){ console.log(res) })
     .then(resp => resp.json())
-        .then(respObj =>{
-            console.log('Iniciou');
-            console.log(respObj)
-        })
+    .then(respObj =>{
+        token = respObj.access_token;
+        //console.log(respObj)
+
+
+        fetch(url,
+            {
+                headers: {
+                    'Authorization': token,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "GET",
+                //body: JSON.stringify({usuario})
+            })
+        //.then(function(res){ console.log(res) })
+            .then(resp => resp.json())
+            .then(respObj =>{
+                //console.log(token);
+                respObj.forEach(function (result) {
+                     html += "<tr><td>"+result.Placa+"</td> <td>"+result.UFPlaca+"</td> </tr>";
+
+                });
+                $('#tabela').append(html);
+                //console.log(respObj)
+            })
+            .catch(error => console.log('Deu Erro '+error));
+
+
+    })
     .catch(error => console.log('Deu Erro '+error));
+
+
+
+
 
     //console.log('Finalizou');
 //}
